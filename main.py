1d7a7883777df8197fd7174c4de3ca1718bb0318
szcohen <91360624+szcohen@users.noreply.github.com>
2021-09-24 22:22:15 -0700

    added initial code

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+
+def findWinner(player1, player2):
+    if player1 == "rock":
+        if scissors blah blah blah
+
+
+    return winner
+
+def oneplayergame():
+    botChoice = random.randrange(1, 3)
+    winner = findWinner(player1choice, botChoice)
+    return
+
+def twoplayergame():
+    player2 = input("player2 please enter your name : ")
+    print("Hello " + player1 + " and " + player2)
+    return
+
+NumOfPlayer = int(input("Would you like to play with 1 or 2 players? : "))
+
+player1 = input("player1 please enter your name : ")
+if NumOfPlayer == 2:
+    twoplayergame()
+elif NumOfPlayer == 1:
+    oneplayergame()
+else:
+    print("nice try guy")
+
+
+
